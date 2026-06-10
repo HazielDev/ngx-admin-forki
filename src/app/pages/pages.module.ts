b1defef2f0a3ece13448fc539523dc94696ae0cc
orlando-ctrl <orlando@soulft.mx>
2026-06-10 17:12:24 -0600

    feat(shared): :sparkles: agregar componente de tabla reutilizable

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { NbMenuModule, NbSpinnerModule, NbCardModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
@@ -8,12 +8,15 @@ import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 import { ProductsComponent } from './products/products/products.component';
+import { CustomTableComponent } from './shared/custom-table/custom-table.component';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
+    NbSpinnerModule,
+    NbCardModule,
     DashboardModule,
     ECommerceModule,
     MiscellaneousModule,
@@ -21,6 +24,7 @@ import { ProductsComponent } from './products/products/products.component';
   declarations: [
     PagesComponent,
     ProductsComponent,
+    CustomTableComponent,
   ],
 })
 export class PagesModule {
